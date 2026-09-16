a9f1dc966f002b55c7ecef2f710a915fa0770e92
Gosfist <akunsekolah1234567@gmail.com>
2026-09-16 20:55:42 +0700

    up 1

--- frontend/src/app/pages/layout/header/header.component.ts
+++ frontend/src/app/pages/layout/header/header.component.ts
@@ -30,6 +30,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     '/dashboard/mainCore/odp': 'ODP',
     '/dashboard/pppoe/packages': 'Daftar Paket PPPoE',
     '/dashboard/pppoe/accounts': 'Akun PPPoE',
+    '/dashboard/pppoe/ip-pools': 'IP Pool',
     '/dashboard/tools/attenuation-calculator': 'Kalkulator Redaman',
   };
 
